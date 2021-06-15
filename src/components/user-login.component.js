5b36c7263e2a08566f6406e8131d262f7443be23
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2021-06-14 23:24:18 -0700

    update

--- src/components/user-login.component.js
+++ src/components/user-login.component.js
@@ -69,7 +69,7 @@ export default class UserLogin extends Component {
                 window.cookie = `username=${credentials.username}`;
 
                 // If the user is authenticated, then redirect them to home page
-                document.location.href = '/home';
+                history.push('/home');
 
             } else if(authentication === false) {
                 // Grabs the paragraph element from the dom based on it's ID
