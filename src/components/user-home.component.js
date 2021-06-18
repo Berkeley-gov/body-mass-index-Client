2f473b8a5da720637deaf62d141e66242fa479d2
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2021-06-18 00:08:49 -0700

    new update okok

--- src/components/user-home.component.js
+++ src/components/user-home.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from "axios";
 
 export default class UserHome extends Component {
     // Constructor for UserHome instances
@@ -22,6 +23,14 @@ export default class UserHome extends Component {
     componentDidMount() {
         let sessionMemory = sessionStorage.getItem('username');
         console.log('the cookie worked - username is: ' + sessionMemory);
+
+        let username = {
+            username: sessionMemory
+        };
+
+        axios.get('https://body-mass-index-cal.herokuapp.com/find', username)
+            .then(response => console.log(response))
+            .catch(error => console.log('+ Failed to make the axios post request to add user to the database: ' + error));
     }
 
     // Function sets the state for the heightInFeet property
@@ -46,6 +55,7 @@ export default class UserHome extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
+
 
         let BMI = this.calculateBodyMassIndex(this.state.heightInFeet, this.state.heightInInches, this.state.weight);
         console.log(`\n> BMI was calculated to approximately:  ${BMI.toPrecision(4)}`);
