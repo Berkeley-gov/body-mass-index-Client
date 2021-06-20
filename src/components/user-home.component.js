23d0f84638e2e1ce6cc655ac9a6b9da8e7d26098
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2021-06-19 23:07:27 -0700

    finished some UI changes to the app

--- src/components/user-home.component.js
+++ src/components/user-home.component.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 export default class UserHome extends Component {
     // Constructor for UserHome instances
@@ -81,137 +82,143 @@ export default class UserHome extends Component {
 
     
     render() {
-        return (
-            <div className="container-fluid fs-5 lh-base">
-                <main className="row" style={{ margin: '100px 0'}}>
-                    {/* start of the welcome board section of the home page  */}
-                    <h2 className="col-md-12 text-center fs-1 mb-5">Welcome {this.state.userLoggedIn.first_name}!</h2>
-                    <p className="text-center fs-4" style={{ marginBottom: '60px' }}>Enter your height and weight below to find your body mass index (BMI). This number is commonly used to judge whether your weight is healthy.</p>
+        if(this.state.userLoggedIn !== null) {
+            return(
+                <div><p className="text-center">Please Log in. <Link to="/login" className="nav-link active">Go back.</Link></p></div>
+            )
+        } else{
+            return (
+                <div className="container-fluid fs-5 lh-base">
+                    <main className="row" style={{ margin: '100px 0'}}>
+                        {/* start of the welcome board section of the home page  */}
+                        <h2 className="col-md-12 text-center fs-1 mb-5">Welcome {this.state.userLoggedIn.first_name}!</h2>
+                        <p className="text-center fs-4" style={{ marginBottom: '60px' }}>Enter your height and weight below to find your body mass index (BMI). This number is commonly used to judge whether your weight is healthy.</p>
 
-                    <section className="col-md-6 will">
-                        <div className="card mb-5 shadow p-1">
-                            <div className="row no-gutters">
-                                <div className="col-md-4">
-                                    <img src="https://cdn2.iconfinder.com/data/icons/men-women-health-wildberry-vol-1/256/BMI_Calculator-512.png" className="card-img shadow-sm" alt="" />
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h5 className="card-title">Body Mass Index</h5>
-                                        <hr />
-                                        <p className="card-text">Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women.</p>
-                                        <p className="card-text">Your BMI can be calculated using standard or metric measures for your height and weight.</p>
-                                        <p className="card-text"><small className="text-muted">Last updated 3 mins
-                                            ago</small></p>
+                        <section className="col-md-6 will">
+                            <div className="card mb-5 shadow p-1">
+                                <div className="row no-gutters">
+                                    <div className="col-md-4">
+                                        <img src="https://cdn2.iconfinder.com/data/icons/men-women-health-wildberry-vol-1/256/BMI_Calculator-512.png" className="card-img shadow-sm" alt="" />
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="card shadow p-1 mt-5 mb-5">
-                            <div className="row no-gutters">
-                                <div className="col">
-                                    <div className="card-body">
-                                        <h5 className="card-title text-center pb-2">Standardized cutoff points for overweight and obesity:</h5>
-
-                                        <ul className="list-group bg-body p-2">
-                                            <li className="list-group-item d-flex justify-content-between align-items-center bg-success text-white">
-                                                Normal weight is a BMI between 18.5 and 24.9
-                                                <span className="badge badge-primary badge-pill bg-white text-success">18.5 - 24.9</span>
-                                            </li>
-                                            <li className="list-group-item d-flex justify-content-between align-items-center bg-warning text-white">
-                                                Overweight is a BMI between 25.0 and 29.9
-                                                <span className="badge badge-primary badge-pill bg-white text-warning"> 25.0 - 29.9</span>
-                                            </li>
-                                            <li className="list-group-item d-flex justify-content-between align-items-center bg-danger text-white">
-                                                Obesity is a BMI of 30.0 or higher
-                                                <span className="badge badge-primary badge-pill bg-white text-danger">30</span>
-                                            </li>
-                                        </ul>
-
-                                        <br />
-                                        <p className="card-text text-center"><small className="text-muted text-center">Last updated 3 mins ago</small></p>
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Body Mass Index</h5>
+                                            <hr />
+                                            <p className="card-text">Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women.</p>
+                                            <p className="card-text">Your BMI can be calculated using standard or metric measures for your height and weight.</p>
+                                            <p className="card-text"><small className="text-muted">Last updated 3 mins
+                                                ago</small></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </section>
 
-                    <section className="col-md-6 will">
-                        <div className="card shadow">
-                            <div className="card-header text-center bg-dark text-white">
-                                BMI Calculator
-                            </div>
-                            <div className="card-body p-5">
-                                <p className="card-text text-center">The BMI calculator provides body mass index and the corresponding weight status category for adults 20 years and older.</p>
+                            <div className="card shadow p-1 mt-5 mb-5">
+                                <div className="row no-gutters">
+                                    <div className="col">
+                                        <div className="card-body">
+                                            <h5 className="card-title text-center pb-2">Standardized cutoff points for overweight and obesity:</h5>
 
-                                <form onSubmit={this.onSubmit}>
-                                    <br/>
-                                    <p className="card-text pb-3"><b>Step 1.) Enter your height</b></p>
-                                    <div className="row">
-                                        <div className="col-6">
-                                            <label>Enter feet:</label>
-                                            <input
-                                                type="number"
-                                                value={this.state.heightInFeet}
-                                                className="form-control"
-                                                onChange={this.onChangeFeet}
-                                                placeholder="5"
-                                                autoComplete="on"
-                                                required
-                                            />
-                                        </div>
+                                            <ul className="list-group bg-body p-2">
+                                                <li className="list-group-item d-flex justify-content-between align-items-center bg-success text-white">
+                                                    Normal weight is a BMI between 18.5 and 24.9
+                                                    <span className="badge badge-primary badge-pill bg-white text-success">18.5 - 24.9</span>
+                                                </li>
+                                                <li className="list-group-item d-flex justify-content-between align-items-center bg-warning text-white">
+                                                    Overweight is a BMI between 25.0 and 29.9
+                                                    <span className="badge badge-primary badge-pill bg-white text-warning"> 25.0 - 29.9</span>
+                                                </li>
+                                                <li className="list-group-item d-flex justify-content-between align-items-center bg-danger text-white">
+                                                    Obesity is a BMI of 30.0 or higher
+                                                    <span className="badge badge-primary badge-pill bg-white text-danger">30</span>
+                                                </li>
+                                            </ul>
 
-                                        <div className="col-6">
-                                            <label>Enter inches:</label>
-                                            <input
-                                                type="number"
-                                                value={this.state.heightInInches}
-                                                className="form-control"
-                                                onChange={this.onChangeInches}
-                                                placeholder="4"
-                                                autoComplete="on"
-                                                required
-                                            />
+                                            <br />
+                                            <p className="card-text text-center"><small className="text-muted text-center">Last updated 3 mins ago</small></p>
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                        </section>
 
-                                    <br/>
-                                    <p className="card-text pb-3 pt-3"><b>Step 2.) Enter your weight</b></p>
-                                    <div className="row">
-                                        <div className="col-6">
-                                            <label>Enter weight in pounds:</label>
-                                            <input
-                                                type="number"
-                                                className="form-control"
-                                                value={this.state.weight}
-                                                placeholder="135.8"
-                                                onChange={this.onChangeWeight}
-                                                autoComplete="on"
-                                                required
-                                            />
-                                        </div>
+                        <section className="col-md-6 will">
+                            <div className="card shadow">
+                                <div className="card-header text-center bg-dark text-white">
+                                    BMI Calculator
+                                </div>
+                                <div className="card-body p-5">
+                                    <p className="card-text text-center">The BMI calculator provides body mass index and the corresponding weight status category for adults 20 years and older.</p>
 
-                                        <div className="col-6">
-                                            <div id="bodyMassIndex" className="p-4 text-center text-white">
+                                    <form onSubmit={this.onSubmit}>
+                                        <br/>
+                                        <p className="card-text pb-3"><b>Step 1.) Enter your height</b></p>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <label>Enter feet:</label>
+                                                <input
+                                                    type="number"
+                                                    value={this.state.heightInFeet}
+                                                    className="form-control"
+                                                    onChange={this.onChangeFeet}
+                                                    placeholder="5"
+                                                    autoComplete="on"
+                                                    required
+                                                />
+                                            </div>
 
+                                            <div className="col-6">
+                                                <label>Enter inches:</label>
+                                                <input
+                                                    type="number"
+                                                    value={this.state.heightInInches}
+                                                    className="form-control"
+                                                    onChange={this.onChangeInches}
+                                                    placeholder="4"
+                                                    autoComplete="on"
+                                                    required
+                                                />
                                             </div>
                                         </div>
 
-                                        <input type="submit" className="btn btn-lg btn-outline-success mt-5" value="Calculate"/>
-                                    </div>
-                                </form>
-                            </div>
+                                        <br/>
+                                        <p className="card-text pb-3 pt-3"><b>Step 2.) Enter your weight</b></p>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <label>Enter weight in pounds:</label>
+                                                <input
+                                                    type="number"
+                                                    className="form-control"
+                                                    value={this.state.weight}
+                                                    placeholder="135.8"
+                                                    onChange={this.onChangeWeight}
+                                                    autoComplete="on"
+                                                    required
+                                                />
+                                            </div>
 
-                            <p className="card-text text-center text-muted"><small>Note: this calculator uses JavaScript. If you have JavaScript turned off or have problems using the calculator, use the formula for calculating BMI on <a href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html">About BMI for Adults</a>.</small></p>
-                            <div className="card-footer text-center bg-dark text-white">
-                                2 days ago
-                            </div>
-                        </div>
-                    </section>
+                                            <div className="col-6">
+                                                <div id="bodyMassIndex" className="p-4 text-center text-white">
 
-                </main>
-            </div>
-        );
+                                                </div>
+                                            </div>
+
+                                            <input type="submit" className="btn btn-lg btn-outline-success mt-5" value="Calculate"/>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <p className="card-text text-center text-muted"><small>Note: this calculator uses JavaScript. If you have JavaScript turned off or have problems using the calculator, use the formula for calculating BMI on <a href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html">About BMI for Adults</a>.</small></p>
+                                <div className="card-footer text-center bg-dark text-white">
+                                    2 days ago
+                                </div>
+                            </div>
+                        </section>
+
+                    </main>
+                </div>
+            );
+        }
     }
 }
 
