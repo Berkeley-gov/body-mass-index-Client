f2a45e622608b907a6caf127ffad885aa4675c67
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2021-06-25 00:37:54 -0700

    updated login page

--- src/components/user-login.component.js
+++ src/components/user-login.component.js
@@ -123,7 +123,7 @@ export default class UserLogin extends Component {
                             <p id="warning-text"></p>
 
                             <div className="nav-item">
-                                <a href="#" className="text-warning stretched-link" onClick={()=> this.props.history.push('/users/add')} >Create an account?</a>
+                                <a href="" className="text-warning stretched-link" onClick={()=> this.props.history.push('/users/add')} >Create an account?</a>
                             </div>
 
                             <div  className="form-group mt-2 pt-4" >
