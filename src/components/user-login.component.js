c86523d122e30e6cfef95014839682b8abd63114
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2021-06-25 00:33:24 -0700

    updated login page

--- src/components/user-login.component.js
+++ src/components/user-login.component.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {Link} from "react-router-dom";
 
 export default class UserLogin extends Component {
     constructor(props) {
